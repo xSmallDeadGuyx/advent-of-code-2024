0eecaf107332f13e9b8bac56cd232b512430650f
xSmallDeadGuyx <alex.gilday@gmail.com>
2024-11-23 13:25:03 +0000

    Add custom content importer for input files

---
+++ AoC2024InputContent/AoC2024InputContent/AoCInputImporter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace AoC2024InputContent;
+
+[ContentImporter(".txt", DisplayName = "AoCInputImporter", DefaultProcessor = "AoCInputProcessor")]
+public class AoCInputImporter : ContentImporter<string>
+{
+    public override string Import(string filename, ContentImporterContext context)
+    {
+        return File.ReadAllText(filename);
+    }
+}
---
+++ AoC2024InputContent/AoC2024InputContent/AoCInputProcessor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace AoC2024InputContent;
+
+[ContentProcessor(DisplayName = "AoCInputProcessor")]
+class AoCInputProcessor : ContentProcessor<string, string>
+{
+    public override string Process(string input, ContentProcessorContext context)
+    {
+        return input;
+    }
+}
