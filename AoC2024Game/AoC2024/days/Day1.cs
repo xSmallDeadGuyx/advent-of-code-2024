7dff76f26646673a6e1535877e57dcfcb2d4303b
xSmallDeadGuyx <alex.gilday@gmail.com>
2024-12-03 12:35:08 +0000

    Ignore AoC inputs for git

--- AoC2024Game/AoC2024/days/Day1.cs
+++ AoC2024Game/AoC2024/days/Day1.cs
@@ -16,11 +16,11 @@ public class Day1 : DayBase
 
     public override void Draw(GameTime gameTime, ref SpriteBatch spriteBatch)
     {
-        spriteBatch.DrawString(_game.hack20, "Day 1 drawing!", new Vector2(20, 20), Color.Black);
+        spriteBatch.DrawString(_game.hack20, "Day 1 drawing! Input = " + _input, new Vector2(20, 20), Color.Black);
     }
 
     public override void Update(GameTime gameTime)
     {
-        Debug.WriteLine(_input);
+        Debug.WriteLine("Input = " + _input);
     }
 }
